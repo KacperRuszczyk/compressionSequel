0607cea6b905ed723762dcf902fe5f0cad9f1ac8
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-02-05 19:35:16 +0100

    Add files via upload

--- compressionPlot.py
+++ compressionPlot.py
@@ -102,8 +102,8 @@ st.markdown('gzip, bzip2, xz, help')
 
 st.markdown(os.listdir('/mount/src/compressionsequel/work_space/results_dir'))
 
-st.markdown(os.path.getsize('/mount/src/compressionsequel/work_space/results_dir/results.csv'))
-subprocess.run(['bzip2', '/mount/src/compressionsequel/work_space/results_dir/results.csv'])
-st.markdown(os.listdir('/mount/src/compressionsequel/work_space/results_dir'))
-st.markdown(os.path.getsize('/mount/src/compressionsequel/work_space/results_dir/results.csv.bz2'))
+#st.markdown(os.path.getsize('/mount/src/compressionsequel/work_space/results_dir/results.csv'))
+#subprocess.run(['bzip2', '/mount/src/compressionsequel/work_space/results_dir/results.csv'])
+#st.markdown(os.listdir('/mount/src/compressionsequel/work_space/results_dir'))
+#st.markdown(os.path.getsize('/mount/src/compressionsequel/work_space/results_dir/results.csv.bz2'))
 
