fbd0eda90fff5b294b2dd2c71db8af3aa6f90745
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-02-08 18:46:34 +0100

    Add files via upload

--- compressionPlot.py
+++ compressionPlot.py
@@ -1,14 +1,65 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
+import os
+import subprocess
+
+import my_functions
+
+#streamlit boot PATH creation
+if os.path.exists('/mount/src/compressionsequel/work_space') == False:
+    os.mkdir('/mount/src/compressionsequel/work_space')
+    os.mkdir('/mount/src/compressionsequel/work_space/data_dir')
+    os.mkdir('/mount/src/compressionsequel/work_space/compressed_dir')
+    os.mkdir('/mount/src/compressionsequel/work_space/decompressed_dir')
+    os.mkdir('/mount/src/compressionsequel/work_space/results_dir')
+    
+    
+
+uploaded_file = st.file_uploader("Upload your file here...", type=['csv'])
+
+PATH = 'E:\Py\Data\wyniki1.csv'
+
+col1, left, col2, right, col3 = st.columns([1,0.1,1,0.1,1])
+
+metods = []
+decomp_metodes = []
+
+
+with col1:
+    one_check = st.checkbox('Gzip')
+    if one_check:
+        metods.append('gzip')  
+        decomp_metodes.append('gunzip')
+with col2:
+    two_check = st.checkbox('Bzip2')
+    if two_check:
+        metods.append('bzip2')       
+        decomp_metodes.append(['bzip2','-d'])
+with col3:
+    three_check = st.checkbox('Xz')
+    if three_check:
+        metods.append('xz') 
+        decomp_metodes.append('unxz')
+with col1:
+    four_check = st.checkbox('Gzip 2')
+    if four_check:
+        metods.append('Gzip 2')        
+with col2:
+    five_check = st.checkbox('L4 2')
+    if five_check:
+        metods.append('L4 2')        
+with col3:
+    six_check = st.checkbox('Xz 2')
+    if six_check:
+        metods.append('Xz 2')        
 
 
 
-data = pd.read_csv('E:\Py\Data\wyniki1.csv' , sep = ',')
+st.markdown(f''' :red[methods used:] :gray[{str(metods)}]''')
 
-data['CompressionFactor'] = 100 - (100 * data['compressedFileSize'] / data['sizeBefore'])
+data = my_functions.loadData(uploaded_file)
 averageTime = data['compressionTime'].mean()
-
 unique_methods = list(set(data['method']))
 
 meanCompressionFactor = []
@@ -17,39 +68,75 @@ meanDecompressionTime = []
 
 for method in unique_methods:
     mask = data['method'] == method
-    meanCompressionFactor.append(data['CompressionFactor'][mask].mean())
+    meanCompressionFactor.append(data['compressionFactor'][mask].mean())
     meanCompressionTime.append(data['compressionTime'][mask].mean())
     meanDecompressionTime.append(data['decompressionTime'][mask].mean())
 
-def resultDataFrame():
-    result = pd.DataFrame({
-        'Method': unique_methods,
-        'Compression Factor': meanCompressionFactor,
-        'Compression Time (s)': meanCompressionTime,
-        'Decompression Time (s)': meanDecompressionTime
-    })
-    return result
 
-st.title('Charts')
-col1, left, col2, right, col3 = st.columns([1,0.1,1,0.1,1])
+
+
+col1, left, col2, center, col3, right, col4 = st.columns([1, 0.1, 1, 0.1, 1, 0.1, 1])
 
 with col1:
-    page1 = st.button("Look at a graph")
+    page1 = st.button("Graphs")
 
 with col2:
-    page2 = st.button("Look at an image")
+    page2 = st.button("Data")
 
 with col3:
-    page3 = st.button("The writing on the wall")
+    page3 = st.button("One graph")
+    
+with col4:
+    page4 = st.button("CPU inf")
 
 if page1:
+    st.title('Graphs')
     st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Compression Factor': meanCompressionFactor}), x='Method', y='Compression Factor')
     st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Compression Time (s)': meanCompressionTime}), x='Method', y='Compression Time (s)')
     st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Decompression Time (s)': meanDecompressionTime}), x='Method', y='Decompression Time (s)')
 
 if page2:
-    st.dataframe(resultDataFrame())
+    st.title('Data Frame')
+    st.dataframe(my_functions.result_data_frame(unique_methods,meanCompressionFactor,meanCompressionTime,meanDecompressionTime))
 
 if page3:
-    st.subheader("You won't be able to nest any other widgets after using the button!")
-    st.subheader("The memory of a button click lasts only 1 script run")
+    st.title('OG Graph ')
+    st.pyplot(my_functions.Graph_with_dots(data))
+    
+if page4:
+    st.title('CPU ')
+    st.markdown(subprocess.run(['lscpu','-C','cpu'], shell=True, capture_output=True, text=True))
+    
+    
+st.markdown(os.getcwd())
+
+st.markdown('gzip, bzip2, xz, help')
+
+st.markdown(os.listdir('/mount/src/compressionsequel/work_space'))
+
+#st.markdown(os.path.getsize('/mount/src/compressionsequel/work_space/results_dir/results.csv'))
+#subprocess.run(['bzip2', '/mount/src/compressionsequel/work_space/results_dir/results.csv'])
+#st.markdown(os.listdir('/mount/src/compressionsequel/work_space/results_dir'))
+#st.markdown(os.path.getsize('/mount/src/compressionsequel/work_space/results_dir/results.csv.bz2'))
+
+
+st.markdown(os.path.exists( '/mount/src/compressionsequel/compressionPlot.py'))
+X = '/mount/src/compressionsequel/compressionPlot.py'
+
+result_temp=subprocess.run([f"ls -l {X} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+st.markdown(result_temp)
+
+file_size = result_temp.stdout.strip()
+
+st.markdown(file_size)
+
+#st.markdown(os.listdir('/mount/src/compressionsequel/work_space/compressed_dir'))
+#subprocess.run(['bzip2', '/mount/src/compressionsequel/work_space/compressed_dir/compressionPlot.py'])
+##subprocess.run(['rm', '/mount/src/compressionsequel/work_space/compressed_dir/compressionPlot.py'])
+st.markdown(os.listdir('/mount/src/compressionsequel/work_space/compressed_dir'))
+
+st.markdown(os.path.isfile('/mount/src/compressionsequel/work_space/compressed_dir/compressionPlot.py.bz2'))
+
+st.markdown(subprocess.run(['bzip2','-d', '/mount/src/compressionsequel/work_space/compressed_dir/compressionPlot.py.bz2'], capture_output=True, text=True))
+
+st.markdown(os.listdir('/mount/src/compressionsequel/work_space/compressed_dir'))
---
+++ compressionScript.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import subprocess
+
+
+def compression(metods, decomp_metodes):
+    # Data variables
+    files = [] #1
+    comp_metode = [] #2
+    file_size = [] #3
+    comp_time = [] #4
+    file_size_after_comp = [] #5
+    decomp_time = [] #6
+    file_size_after_decomp = [] #7
+    check_if_diff = [] #8
+
+
+    # Paths
+    data_dir = '/mount/src/compressionsequel/work_space/data_dir'
+    compressed_dir = '/mount/src/compressionsequel/work_space/compressed_dir'
+    decompressed_dir = '/mount/src/compressionsequel/work_space/decompressed_dir'
+    results_dir = '/mount/src/compressionsequel/work_space/results_dir'
+
+    i = 0
+
+
+
+    for metod in metods:
+        for file_name in os.listdir(data_dir):
+            path_with_file_name = os.path.join(data_dir, file_name)
+            
+            files.append(file_name) #1
+            comp_metode.append(metod) #2
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size.append(result_temp2) #3
+            
+            start_time = time.time()    
+            subprocess.run([metod, path_with_file_name])
+            end_time = time.time()
+            comp_time.append(end_time - start_time) #4
+            
+            subprocess.run(['rm', path_with_file_name])
+            
+        
+        subprocess.run(['mv', f'{data_dir}/*', compressed_dir])
+        
+        
+        for file_name in os.listdir(compressed_dir):
+            path_with_file_name = os.path.join(compressed_dir, file_name)
+            
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size_after_comp.append(result_temp2) #5
+                
+            start_time = time.time()    
+            if decomp_metodes == list:
+                subprocess.run([decomp_metodes[i][0],decomp_metodes[i][1], path_with_file_name], shell=True)
+            else:
+                subprocess.run([decomp_metodes[i], path_with_file_name], shell=True)
+            end_time = time.time()
+            decomp_time.append(end_time - start_time) #6
+            
+            
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size_after_decomp.append(result_temp2) #7
+            
+            subprocess.run(['rm', path_with_file_name])
+            
+        subprocess.run(['mv', f'{compressed_dir}/*', decompressed_dir])    
+        i =+ 1
+        
+        for file_name in os.listdir(decompressed_dir):
+            check_if_diff.append(subprocess.run(['diff', '-s', f'{decompressed_dir}/{file_name}', f'{data_dir}/{file_name}', '|', 'awk', '{{print $6}}'], shell=True, capture_output=True, text=True))  #8
+        
+        
+
+    results = os.path.join(results_dir, 'results.csv')
+
+    with open(results, 'a') as file:
+        file.write("method;filename;size_before;compression_time;size_after_compression;decompression_time;size_after_decompression;are_identical\n")
+
+        for i in range(len(cmethods)):    
+            file.write(f"{comp_metode[i]};{files[i]};{file_size[i]};{comp_time[i]};{file_size_after_comp[i]};{decomp_time[i]};{file_size_after_decomp[i]};{check_if_diff[i]}\n")
+    return
