59f5f3427971bdb7cd03f53de4a9a38ac4cbe7ab
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-02-08 17:27:43 +0100

    Add files via upload

--- compressionScript.py
+++ compressionScript.py
@@ -3,79 +3,82 @@ import os
 import time
 import subprocess
 
-# Data variables
-files = [] #1
-comp_metode = [] #2
-file_size = [] #3
-comp_time = [] #4
-file_size_after_comp = [] #5
-decomp_time = [] #6
-file_size_after_decomp = [] #7
-check_if_diff = [] #8\\
+
+def compression(metods, decopm_metodes):
+    # Data variables
+    files = [] #1
+    comp_metode = [] #2
+    file_size = [] #3
+    comp_time = [] #4
+    file_size_after_comp = [] #5
+    decomp_time = [] #6
+    file_size_after_decomp = [] #7
+    check_if_diff = [] #8
 
 
-# Paths
-data_dir = '/mount/src/compressionsequel/work_space/data_dir'
-compressed_dir = '/mount/src/compressionsequel/work_space/compressed_dir'
-decompressed_dir = '/mount/src/compressionsequel/work_space/decompressed_dir'
-results_dir = '/mount/src/compressionsequel/work_space/results_dir'
+    # Paths
+    data_dir = '/mount/src/compressionsequel/work_space/data_dir'
+    compressed_dir = '/mount/src/compressionsequel/work_space/compressed_dir'
+    decompressed_dir = '/mount/src/compressionsequel/work_space/decompressed_dir'
+    results_dir = '/mount/src/compressionsequel/work_space/results_dir'
 
-i = 0
+    i = 0
 
 
 
-for metod in metods:
-    for file_name in os.listdir(data_dir):
-        path_with_file_name = os.path.join(data_dir, file_name)
-        
-        files.append(file_name) #1
-        comp_metode.append(metod) #2
-        result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
-        result_temp2 = result_temp.stdout.strip()
-        file_size.append(result_temp2) #3
-        
-        start_time = time.time()    
-        subprocess.run([metod, path_with_file_name])
-        end_time = time.time()
-        comp_time.append(end_time - start_time) #4
-        
-        subprocess.run(['rm', path_with_file_name])
-        
-    
-    subprocess.run(['mv', f'{data_dir}/*', compressed_dir])
-    
-    
-    for file_name in os.listdir(compressed_dir):
-        path_with_file_name = os.path.join(compressed_dir, file_name)
-        
-        result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
-        result_temp2 = result_temp.stdout.strip()
-        file_size_after_comp.append(result_temp2) #5
+    for metod in metods:
+        for file_name in os.listdir(data_dir):
+            path_with_file_name = os.path.join(data_dir, file_name)
             
-        start_time = time.time()    
-        subprocess.run([decopm_metodes[i], path_with_file_name], shell=True)
-        end_time = time.time()
-        decomp_time.append(end_time - start_time) #6
+            files.append(file_name) #1
+            comp_metode.append(metod) #2
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size.append(result_temp2) #3
+            
+            start_time = time.time()    
+            subprocess.run([metod, path_with_file_name])
+            end_time = time.time()
+            comp_time.append(end_time - start_time) #4
+            
+            subprocess.run(['rm', path_with_file_name])
+            
+        
+        subprocess.run(['mv', f'{data_dir}/*', compressed_dir])
         
         
-        result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
-        result_temp2 = result_temp.stdout.strip()
-        file_size_after_decomp.append(result_temp2) #7
+        for file_name in os.listdir(compressed_dir):
+            path_with_file_name = os.path.join(compressed_dir, file_name)
+            
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size_after_comp.append(result_temp2) #5
+                
+            start_time = time.time()    
+            subprocess.run([decopm_metodes[i], path_with_file_name], shell=True)
+            end_time = time.time()
+            decomp_time.append(end_time - start_time) #6
+            
+            
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size_after_decomp.append(result_temp2) #7
+            
+            subprocess.run(['rm', path_with_file_name])
+            
+        subprocess.run(['mv', f'{compressed_dir}/*', decompressed_dir])    
+        i =+ 1
         
-        subprocess.run(['rm', path_with_file_name])
+        for file_name in os.listdir(decompressed_dir):
+            check_if_diff.append(subprocess.run(['diff', '-s', f'{decompressed_dir}/{file_name}', f'{data_dir}/{file_name}', '|', 'awk', '{{print $6}}'], shell=True, capture_output=True, text=True))  #8
         
-    subprocess.run(['mv', f'{compressed_dir}/*', decompressed_dir])    
-    i =+ 1
-    
-    for file_name in os.listdir(decompressed_dir):
-        check_if_diff.append(subprocess.run(['diff', '-s', f'{decompressed_dir}/{file_name}', f'{data_dir}/{file_name}', '|', 'awk', '{{print $6}}'], shell=True, capture_output=True, text=True))  #8
-    
-    
+        
 
-results = os.path.join(results_dir, 'results.csv')
+    results = os.path.join(results_dir, 'results.csv')
 
-with open(results, 'a') as file:
-    file.write("method;filename;size_before;compression_time;size_after_compression;decompression_time;size_after_decompression;are_identical\n")
+    with open(results, 'a') as file:
+        file.write("method;filename;size_before;compression_time;size_after_compression;decompression_time;size_after_decompression;are_identical\n")
 
-    for i in range(len(cmethods)):    
-        file.write(f"{comp_metode[i]};{files[i]};{file_size[i]};{comp_time[i]};{file_size_after_comp[i]};{decomp_time[i]};{file_size_after_decomp[i]};{check_if_diff[i]}\n")
+        for i in range(len(cmethods)):    
+            file.write(f"{comp_metode[i]};{files[i]};{file_size[i]};{comp_time[i]};{file_size_after_comp[i]};{decomp_time[i]};{file_size_after_decomp[i]};{check_if_diff[i]}\n")
+    return
