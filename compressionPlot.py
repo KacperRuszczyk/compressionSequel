2447369af1de57f01cbf59eae2515b93053778b4
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-02-09 19:27:33 +0100

    Add files via upload

--- compressionPlot.py
+++ compressionPlot.py
@@ -17,91 +17,92 @@ if os.path.exists('/mount/src/compressionsequel/work_space') == False:
     
 
 uploaded_files = st.file_uploader("Upload your files here...", accept_multiple_files=True)
-  
-for uploaded_file in uploaded_files:
-    my_functions.save_file(uploaded_file)
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        my_functions.save_file(uploaded_file)
 
 
 
 
 
-col1, left, col2, right, col3 = st.columns([1,0.1,1,0.1,1])
+    col1, left, col2, right, col3 = st.columns([1,0.1,1,0.1,1])
 
-metods = []
-decomp_metodes = []
-
-
-with col1:
-    one_check = st.checkbox('Gzip')
-    if one_check:
-        metods.append('gzip')  
-        decomp_metodes.append('gunzip')
-with col2:
-    two_check = st.checkbox('Bzip2')
-    if two_check:
-        metods.append('bzip2')       
-        decomp_metodes.append(['bzip2','-d'])
-with col3:
-    three_check = st.checkbox('Xz')
-    if three_check:
-        metods.append('xz') 
-        decomp_metodes.append('unxz')
-    
+    metods = []
+    decomp_metodes = []
 
 
-
-st.markdown(f''' :red[methods used:] :gray[{str(metods)}]''')
-
-data = my_functions.loadData(uploaded_file)
-averageTime = data['compressionTime'].mean()
-unique_methods = list(set(data['method']))
-
-meanCompressionFactor = []
-meanCompressionTime = []
-meanDecompressionTime = []
-
-for method in unique_methods:
-    mask = data['method'] == method
-    meanCompressionFactor.append(data['compressionFactor'][mask].mean())
-    meanCompressionTime.append(data['compressionTime'][mask].mean())
-    meanDecompressionTime.append(data['decompressionTime'][mask].mean())
+    with col1:
+        one_check = st.checkbox('Gzip')
+        if one_check:
+            metods.append('gzip')  
+            decomp_metodes.append('gunzip')
+    with col2:
+        two_check = st.checkbox('Bzip2')
+        if two_check:
+            metods.append('bzip2')       
+            decomp_metodes.append(['bzip2','-d'])
+    with col3:
+        three_check = st.checkbox('Xz')
+        if three_check:
+            metods.append('xz') 
+            decomp_metodes.append('unxz')
+        
 
 
 
+    st.markdown(f''' :red[methods used:] :gray[{str(metods)}]''')
 
-col1, left, col2, center, col3, right, col4 = st.columns([1, 0.1, 1, 0.1, 1, 0.1, 1])
+    data = my_functions.loadData(uploaded_file)
+    averageTime = data['compressionTime'].mean()
+    unique_methods = list(set(data['method']))
 
-with col1:
-    page1 = st.button("Graphs")
+    meanCompressionFactor = []
+    meanCompressionTime = []
+    meanDecompressionTime = []
 
-with col2:
-    page2 = st.button("Data")
+    for method in unique_methods:
+        mask = data['method'] == method
+        meanCompressionFactor.append(data['compressionFactor'][mask].mean())
+        meanCompressionTime.append(data['compressionTime'][mask].mean())
+        meanDecompressionTime.append(data['decompressionTime'][mask].mean())
 
-with col3:
-    page3 = st.button("One graph")
-    
-with col4:
-    page4 = st.button("CPU inf")
 
-if page1:
-    st.title('Graphs')
-    st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Compression Factor': meanCompressionFactor}), x='Method', y='Compression Factor')
-    st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Compression Time (s)': meanCompressionTime}), x='Method', y='Compression Time (s)')
-    st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Decompression Time (s)': meanDecompressionTime}), x='Method', y='Decompression Time (s)')
 
-if page2:
-    st.title('Data Frame')
-    st.dataframe(my_functions.result_data_frame(unique_methods,meanCompressionFactor,meanCompressionTime,meanDecompressionTime))
 
-if page3:
-    st.title('OG Graph ')
-    st.pyplot(my_functions.Graph_with_dots(data))
-    
-if page4:
-    st.title('CPU ')
-    st.markdown(subprocess.run(['lscpu','-C','cpu'], shell=True, capture_output=True, text=True))
-    
-    
+    col1, left, col2, center, col3, right, col4 = st.columns([1, 0.1, 1, 0.1, 1, 0.1, 1])
+
+    with col1:
+        page1 = st.button("Graphs")
+
+    with col2:
+        page2 = st.button("Data")
+
+    with col3:
+        page3 = st.button("One graph")
+        
+    with col4:
+        page4 = st.button("CPU inf")
+
+    if page1:
+        st.title('Graphs')
+        st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Compression Factor': meanCompressionFactor}), x='Method', y='Compression Factor')
+        st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Compression Time (s)': meanCompressionTime}), x='Method', y='Compression Time (s)')
+        st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Decompression Time (s)': meanDecompressionTime}), x='Method', y='Decompression Time (s)')
+
+    if page2:
+        st.title('Data Frame')
+        st.dataframe(my_functions.result_data_frame(unique_methods,meanCompressionFactor,meanCompressionTime,meanDecompressionTime))
+
+    if page3:
+        st.title('OG Graph ')
+        st.pyplot(my_functions.Graph_with_dots(data))
+        
+    if page4:
+        st.title('CPU ')
+        st.markdown(subprocess.run(['lscpu','-C','cpu'], shell=True, capture_output=True, text=True))
+else:
+    st.warning("Please upload one or more files to proceed.")      
+        
 st.markdown(os.getcwd())
 
 st.markdown('gzip, bzip2, xz, help')
