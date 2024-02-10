bae93c9a82783e965dea2c6a39a4db196304d406
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-02-10 19:07:26 +0100

    Add files via upload

--- compressionScript.py
+++ compressionScript.py
@@ -83,6 +83,6 @@ def compression(metods, decomp_metodes):
     with open(results, 'a') as file:
         file.write("method;filename;size_before;compression_time;size_after_compression;decompression_time;size_after_decompression;are_identical\n")
 
-        for i in range(len(metods)):    
+        for i in range(len(comp_metode)):    
             file.write(f"{comp_metode[i]};{files[i]};{file_size[i]};{comp_time[i]};{file_size_after_comp[i]};{decomp_time[i]};{file_size_after_decomp[i]};{check_if_diff[i]}\n")
     return
