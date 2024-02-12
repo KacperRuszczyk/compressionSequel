a19e90ceb994d2b33ae479a14548d0ed415f9e53
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-02-12 19:14:29 +0100

    Add files via upload

--- compressionPlot.py
+++ compressionPlot.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import subprocess
 import shutil
+import time
 
 import my_functions
 import compressionScript
@@ -17,6 +18,93 @@ if os.path.exists('/mount/src/compressionsequel/work_space') == False:
     os.mkdir('/mount/src/compressionsequel/work_space/decompressed_dir')
     os.mkdir('/mount/src/compressionsequel/work_space/results_dir')
     os.mkdir('/mount/src/compressionsequel/work_space/uploaded_dir')
+    
+def compression(metods, decomp_metodes):
+    # Data harvesting arrays
+    Files = [] #1
+    comp_metode = [] #2
+    file_size = [] #3
+    comp_time = [] #4
+    file_size_after_comp = [] #5
+    decomp_time = [] #6
+    file_size_after_decomp = [] #7
+    check_if_diff = [] #8
+
+
+    # Paths
+    data_dir = '/mount/src/compressionsequel/work_space/data_dir'
+    compressed_dir = '/mount/src/compressionsequel/work_space/compressed_dir'
+    decompressed_dir = '/mount/src/compressionsequel/work_space/decompressed_dir'
+    results_dir = '/mount/src/compressionsequel/work_space/results_dir'
+    uploaded_dir = '/mount/src/compressionsequel/work_space/uploaded_dir'
+    i = 0
+
+
+
+    for metod in metods:
+        #copy files from uploaded_dir to data_dir
+        files_to_copy = os.listdir(uploaded_dir)
+        for file_name in files_to_copy:
+            source_path = os.path.join(uploaded_dir, file_name)
+            destination_path = os.path.join(data_dir, file_name)
+            shutil.copy(source_path, destination_path)
+            
+        for file_name in os.listdir(data_dir):
+            path_with_file_name = os.path.join(data_dir, file_name)
+            
+            Files.append(file_name) #1
+            comp_metode.append(metod) #2
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size.append(result_temp2) #3
+            
+            start_time = time.time()    
+            subprocess.run([metod, path_with_file_name])
+            end_time = time.time()
+            comp_time.append(end_time - start_time) #4
+            
+            subprocess.run(['rm', path_with_file_name])
+            
+        
+        subprocess.run(['mv', f'{data_dir}/*', compressed_dir])
+        
+        
+        for file_name in os.listdir(compressed_dir):
+            path_with_file_name = os.path.join(compressed_dir, file_name)
+            
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size_after_comp.append(result_temp2) #5
+                
+            start_time = time.time()    
+            if decomp_metodes == list:
+                subprocess.run([decomp_metodes[i][0],decomp_metodes[i][1], path_with_file_name], shell=True)
+            else:
+                subprocess.run([decomp_metodes[i], path_with_file_name], shell=True)
+            end_time = time.time()
+            decomp_time.append(end_time - start_time) #6
+            
+            
+            result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            file_size_after_decomp.append(result_temp2) #7
+            
+            subprocess.run(['rm', path_with_file_name])
+            
+        subprocess.run(['mv', f'{compressed_dir}/*', decompressed_dir])    
+        i += 1
+        
+        for file_name in os.listdir(decompressed_dir):
+            check_if_diff.append(subprocess.run(['diff', '-s', f'{decompressed_dir}/{file_name}', f'{data_dir}/{file_name}', '|', 'awk', '{{print $6}}'], shell=True, capture_output=True, text=True))  #8
+
+    return(Files,
+        comp_metode,
+        file_size, 
+        comp_time,
+        file_size_after_comp,
+        decomp_time,
+        file_size_after_decomp,
+        check_if_diff)
    
 uploaded_files = st.file_uploader("Upload your files here...", accept_multiple_files=True)
 
@@ -69,6 +157,6 @@ if uploaded_files:
     with col2:
         compress_button = st.button('Compress The Files')
         if compress_button:    
-            
+            compression(metods, decomp_metodes)
             st.markdown(Files)
             
