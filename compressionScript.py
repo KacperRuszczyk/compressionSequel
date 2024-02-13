82d35b47201a8c3864294f85a11f3799c2a73b16
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-02-13 20:04:10 +0100

    Add files via upload

--- compressionScript.py
+++ compressionScript.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# compressionScript
+#Debug/compressionScript
 import os
 import time
 import subprocess
-import shutil
+
 
 def compression(metods, decomp_metodes):
     # Data harvesting arrays
-    files = [] #1
+    Files_list = [] #1
     comp_metode = [] #2
     file_size = [] #3
     comp_time = [] #4
@@ -38,7 +38,7 @@ def compression(metods, decomp_metodes):
         for file_name in os.listdir(data_dir):
             path_with_file_name = os.path.join(data_dir, file_name)
             
-            files.append(file_name) #1
+            Files_list.append(file_name) #1
             comp_metode.append(metod) #2
             result_temp = subprocess.run([f"ls -l {path_with_file_name} | awk '{{print $5}}'"], shell=True, capture_output=True, text=True)
             result_temp2 = result_temp.stdout.strip()
@@ -49,11 +49,14 @@ def compression(metods, decomp_metodes):
             end_time = time.time()
             comp_time.append(end_time - start_time) #4
             
-            subprocess.run(['rm', path_with_file_name])
+            os.remove(path_with_file_name)
             
         
-        subprocess.run(['mv', f'{data_dir}/*', compressed_dir])
-        
+        #subprocess.run(['mv', f'{data_dir}/*', compressed_dir])
+        files_to_move = os.listdir(data_dir)
+        for file_name in files_to_move:
+            source_path = os.path.join(data_dir, file_name)
+            shutil.move(source_path, compressed_dir)
         
         for file_name in os.listdir(compressed_dir):
             path_with_file_name = os.path.join(compressed_dir, file_name)
@@ -75,30 +78,22 @@ def compression(metods, decomp_metodes):
             result_temp2 = result_temp.stdout.strip()
             file_size_after_decomp.append(result_temp2) #7
             
-            subprocess.run(['rm', path_with_file_name])
+            os.remove(path_with_file_name)
             
-        subprocess.run(['mv', f'{compressed_dir}/*', decompressed_dir])    
+        #subprocess.run(['mv', f'{compressed_dir}/*', decompressed_dir])  
+        files_to_move = os.listdir(compressed_dir)
+        for file_name in files_to_move:
+            source_path = os.path.join(compressed_dir, file_name)
+            shutil.move(source_path, decompressed_dir)
         i += 1
         
         for file_name in os.listdir(decompressed_dir):
-            check_if_diff.append(subprocess.run(['diff', '-s', f'{decompressed_dir}/{file_name}', f'{data_dir}/{file_name}', '|', 'awk', '{{print $6}}'], shell=True, capture_output=True, text=True))  #8
-        
-        
+            file_after = os.path.join(compressed_dir, file_name)
+            file_before = os.path.join(data_dir, file_name)
+            result_temp = subprocess.run([f"diff -s {file_after} {file_before} | awk '{{print $6}}'"], shell=True, capture_output=True, text=True)
+            result_temp2 = result_temp.stdout.strip()
+            check_if_diff.append(result_temp2)  #8
 
-    #results = os.path.join(results_dir, 'results.csv')
-
-    #with open(results, 'a') as file:
-        #file.write("method;filename;sizeBefore;compressionTime;compressedFileSize;decompressionTime;sizeAfterDecompression;different\n")
-                    
-        #for i in range(len(comp_metode)):    
-            #file.write(f"{comp_metode[i]};{files[i]};{file_size[i]};{comp_time[i]};{file_size_after_comp[i]};{decomp_time[i]};{file_size_after_decomp[i]};{check_if_diff[i]}\n")
-        
-   
-    return(files,
-        comp_metode,
-        file_size, 
-        comp_time,
-        file_size_after_comp,
-        decomp_time,
-        file_size_after_decomp,
-        check_if_diff)
+    return(Files_list)
+    
+    
