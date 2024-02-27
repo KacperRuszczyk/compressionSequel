02b62b87dfd840375a1cfa79b1547f78dc278ab6
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-02-27 18:38:45 +0100

    Add files via upload

--- compressionPlot.py
+++ compressionPlot.py
@@ -10,6 +10,13 @@ import time
 import my_functions
 import compressionScript
 
+
+
+unlock_charts = False
+metods = []
+decomp_metodes = []
+
+
 #streamlit boot PATH creation
 if os.path.exists('/mount/src/compressionsequel/work_space') == False:
     os.mkdir('/mount/src/compressionsequel/work_space')
@@ -21,9 +28,6 @@ if os.path.exists('/mount/src/compressionsequel/work_space') == False:
 
 uploaded_files = st.file_uploader("Upload your files here...", accept_multiple_files=True)
 
-
-metods = []
-decomp_metodes = []
 
 col1, left, col2, right, col3 = st.columns([1,0.1,1,0.1,1])
 
@@ -44,8 +48,6 @@ with col3:
         decomp_metodes.append('unxz')
         
 st.markdown(f''' :red[methods used:] :gray[{str(metods)}]''')
-
-
 
 
 col1, left, col2, right, col3 = st.columns([1,0.1,1,0.1,1])
@@ -74,42 +76,90 @@ with col1:
             path_with_file_name = os.path.join(uploaded_dir, file_name)
             os.remove(path_with_file_name)
 
-with col2:
+with col3:
     comp_button = st.button('compress')
     if comp_button:
-         compressionScript.compression(metods, decomp_metodes)
-
-
-
-
-with col3:
-    conf_button = st.button('upload to data dir')
-    if conf_button:       
         if uploaded_files:
             for uploaded_file in uploaded_files:
-                my_functions.save_file(uploaded_file)     
-           
+                my_functions.save_file(uploaded_file)
+            comp_metode,files_list, \
+            file_size,comp_time, \
+            file_size_after_comp,decomp_time, \
+            file_size_after_decomp,check_if_diff = compressionScript.compression(metods, decomp_metodes)
+            unlock_charts = True
+            averageTime = data['compressionTime'].mean()
+            unique_methods = list(set(data['method']))
+
+            meanCompressionFactor = []
+            meanCompressionTime = []
+            meanDecompressionTime = []
+
+            for method in unique_methods:
+                mask = data['method'] == method
+                meanCompressionFactor.append(data['compressionFactor'][mask].mean())
+                meanCompressionTime.append(data['compressionTime'][mask].mean())
+                meanDecompressionTime.append(data['decompressionTime'][mask].mean())
+
+
+
+
+if unlock_charts:
+    
+    col1, left, col2, center, col3, right, col4 = st.columns([1, 0.1, 1, 0.1, 1, 0.1, 1])
+
+    with col1:
+        page1 = st.button("Graphs")
+
+    with col2:
+        page2 = st.button("Data")
+
+    with col3:
+        page3 = st.button("One graph")
         
-st.markdown('uploaded_dir') 
-st.markdown(os.listdir('/mount/src/compressionsequel/work_space/uploaded_dir'))             
-st.markdown('data_dir')     
-st.markdown(os.listdir('/mount/src/compressionsequel/work_space/data_dir'))
-st.markdown('compressed_dir') 
-st.markdown(os.listdir('/mount/src/compressionsequel/work_space/compressed_dir'))
-st.markdown('decompressed_dir') 
-st.markdown(os.listdir('/mount/src/compressionsequel/work_space/decompressed_dir'))
-st.markdown('results_dir') 
-st.markdown(os.listdir('/mount/src/compressionsequel/work_space/results_dir'))
+    with col4:
+        page4 = st.button("CPU inf")
+
+    if page1:
+        st.title('Graphs')
+        st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Compression Factor': meanCompressionFactor}), x='Method', y='Compression Factor')
+        st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Compression Time (s)': meanCompressionTime}), x='Method', y='Compression Time (s)')
+        st.bar_chart(pd.DataFrame({'Method': unique_methods, 'Decompression Time (s)': meanDecompressionTime}), x='Method', y='Decompression Time (s)')
+
+    if page2:
+        st.title('Data Frame')
+        st.dataframe(my_functions.result_data_frame(unique_methods,meanCompressionFactor,meanCompressionTime,meanDecompressionTime))
+
+    if page3:
+        st.title('OG Graph ')
+        st.pyplot(my_functions.Graph_with_dots(data))
+        
+    if page4:
+        st.title('CPU ')
+        st.markdown(subprocess.run(['lscpu','-C','cpu'], shell=True, capture_output=True, text=True))
+        
+        
+
+    
+
+
+
+
+
+    
 
 col1, left, col2, right, col3 = st.columns([1,0.1,1,0.1,1])
 
 
-with col3:
+with col2:
     ref_button = st.button('ref')
     if ref_button:       
         pass
         
-        
-PATH = '/mount/src/compressionsequel/work_space/results_dir/results.txt'
-if st.button("Download File"):
-    st.markdown(f'<a href="{PATH}" download="results.txt">download</a>', unsafe_allow_html=True)
+       
+
+
+
+       
+#PATH = '/mount/src/compressionsequel/work_space/results_dir/results.txt'
+#if st.button("Download File"):
+    #st.markdown(f'<a href="{PATH}" download="results.txt">download</a>', unsafe_allow_html=True)
