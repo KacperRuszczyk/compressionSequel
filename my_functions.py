1a49bf8839e2f3533fadca715285af9eb1ec38d9
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-04 18:36:44 +0100

    Add files via upload

--- my_functions.py
+++ my_functions.py
@@ -167,7 +167,8 @@ def compression(metods, decomp_metodes):
             
             if os.path.isfile(file_after):
                 os.remove(file_after)
-        my_bar.progress(percent_progress =+ percent_complete, text=progress_text)  
+        my_bar.progress(percent_progress, text=progress_text) 
+        percent_progress =+ percent_complete    
     data = pd.DataFrame({
         'method': comp_metode,
         'filename': files_list,
