b966111172386a4b23271ec57926799201255567
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-05 20:25:06 +0100

    Delete server_information.py

--- server_information.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="CPU info",
-    page_icon="💽",
-)
-
-st.markdown(subprocess.run(['lscpu','-C','cpu'], shell=True, capture_output=True, text=True))
