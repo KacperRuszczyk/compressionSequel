31350da3b7f6aecab6ffbc44cfccc81b166a4d46
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-10 17:37:59 +0100

    Add files via upload

--- pages/server_information.py
+++ pages/server_information.py
@@ -18,20 +18,27 @@ result_disk = subprocess.run(['df', '-h'], shell=True, capture_output=True, text
 
 output_lines_cpu  = result_cpu.stdout.split('\n')
 relevant_info = {
-    'Architecture': output_lines_cpu [0].split(': ')[1],
-    'CPU(s)': output_lines_cpu [5].split(': ')[1],
     'Model name': output_lines_cpu [13].split(': ')[1],
+    'Architecture': output_lines_cpu [0].split(': ')[1],    
     'CPU MHz': '2250',
 }
 
+output_lines_disk = result_disk.stdout.split('\n')
+relevant_info_disk = {
+    'Filesystem': output_lines_disk[1].split()[0],
+    'Size': output_lines_disk[1].split()[1],
+    'Used': output_lines_disk[1].split()[2],
+    'Available': output_lines_disk[1].split()[3],
+    'Use%': output_lines_disk[1].split()[4],
+    'Mounted on': output_lines_disk[1].split()[5],
+}
 
 
-
-
+st.header("CPU Information")
 for key, value in relevant_info.items():
     st.write(f"{key}: {value}")
     
+st.header("Drive Capacity Information")
+for key, value in relevant_info_disk.items():
+    st.write(f"{key}: {value}")   
     
-    
-st.markdown(result_memory)
-st.markdown(result_disk)
