c84e453031d84477948168c9a5b95bc5d0a5ac1b
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-10 17:01:37 +0100

    Add files via upload

--- pages/server_information.py
+++ pages/server_information.py
@@ -3,20 +3,35 @@ import os
 import subprocess
 
 st.set_page_config(
-    page_title="CPU info",
-    page_icon="💽",
+    page_title='CPU info',
+    page_icon='💽',
     initial_sidebar_state='expanded'
 )
 
-result = subprocess.run(['lscpu', '-C', 'cpu'], shell=True, capture_output=True, text=True)
+result_cpu = subprocess.run(['lscpu', '-C', 'cpu'], shell=True, capture_output=True, text=True)
 
-output_lines = result.stdout.split('\n')
+result_memory = subprocess.run(['free', '-h'], shell=True, capture_output=True, text=True)
+
+result_disk = subprocess.run(['df', '-h'], shell=True, capture_output=True, text=True)
+
+
+
+output_lines_cpu  = result_cpu.stdout.split('\n')
 relevant_info = {
-    "Architecture": output_lines[0].split(': ')[1],
-    "CPU(s)": output_lines[5].split(': ')[1],
-    "Model name": output_lines[13].split(': ')[1],
-    "CPU MHz": output_lines[14].split(': ')[1],
+    'Architecture': output_lines_cpu [0].split(': ')[1],
+    'CPU(s)': output_lines_cpu [5].split(': ')[1],
+    'Model name': output_lines_cpu [13].split(': ')[1],
+    'CPU MHz': '2250',
 }
+
+
+
+
 
 for key, value in relevant_info.items():
     st.write(f"{key}: {value}")
+    
+    
+    
+st.markdown(result_memory)
+st.markdown(result_disk)
