35136b1b68467698d2337730ec967e0387840846
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-13 20:00:35 +0100

    Add files via upload

--- compressionPlot.py
+++ compressionPlot.py
@@ -61,4 +61,4 @@ if page1:
 
 switch_page_button = st.button('compression demo')
 if switch_page_button:
-    my_functions.switch_page("compression_demo")
+    my_functions.switch_page("compression demo")
