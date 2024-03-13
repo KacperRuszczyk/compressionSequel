3ca79c706b45a7142a288e98f60a8ce44ec3d86e
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-13 18:32:53 +0100

    Add files via upload

--- compressionPlot.py
+++ compressionPlot.py
@@ -26,10 +26,30 @@ st.sidebar.success("Select page above.")
 
 st.title(':red[Efficiency of Lossless Compression Algorithms]')
 
-st.subheader('Welcome to my app dedicated to showcasing the effectiveness and ineffectiveness of lossless compression algorithms')
+st.subheader('Welcome to my app dedicated to showcasing the effectiveness and ineffectiveness of lossless compression algorithms.')
 
-st.subheader('My platform aims to provide insights into the performance and efficiency of these renowned compression tools.')
+st.subheader('My platform aims to provide insights into the performance and efficiency of renowned compression tools such as Bzip2, gzip, and xz.')
+
+st.subheader('Here You can find things such as:')
+
+left, mid, right = st.columns([1, 0.5, 1])
+
+with left:
+    st.header(':red[Compression app]')
+    st.subheader('That can help you visualise how Bzip2, gzip, and xz are compressing different kinds of file types')
+    st.subheader('Here you can also see how the compression script operates')
+with mid:
+    st.subheader('and')
+
+with right:
+    st.header(':red[Information about Algorithms]')
+    st.subheader('Used in all the three compression programs')
+left, right = st.columns([1, 1])
 
 
 
+Page1 = st.button("Huffmana")
 
+
+if page1:
+    st.image('images/Skrypt.png')
