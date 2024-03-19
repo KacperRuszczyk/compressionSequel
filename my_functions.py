8a2879d370eabfd5d4af6ca759134dc3fe1f2f29
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-19 20:14:01 +0100

    Add files via upload

--- my_functions.py
+++ my_functions.py
@@ -224,6 +224,4 @@ def _get_session():
     runtime = get_instance()
     session_id = get_script_run_ctx().session_id
     session_info = runtime._session_mgr.get_session_info(session_id)
-    if session_info is None:
-        raise RuntimeError("Couldn't get your Streamlit Session object.")
     return session_info.session
