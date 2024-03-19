e9a89b9b1c0a03f618c66429ac70efca03ca0a53
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-19 20:08:34 +0100

    Add files via upload

--- Main_Page.py
+++ Main_Page.py
@@ -24,33 +24,9 @@ st.set_page_config(
     initial_sidebar_state='expanded'
 )
 
-st.sidebar.success("Select page above.")
-
-st.title(':orange[Lossless Compression Algorithms]',anchor=False)
-
-st.subheader('This app showcases the effectiveness and ineffectiveness of lossless compression algorithms.',anchor=False)
-
-st.subheader("Here you'll be able to examine the performance and efficiency of multiple compression tools. See for yourself!",anchor=False)
-
-st.subheader(':arrow_left: Just pick one of the sites on the left.',anchor=False)
-
-left, mid, right = st.columns([1, 0.2, 1])
-
-with left:
-    st.subheader(':orange[Compression app]',anchor=False)
-    st.markdown('Demo that can help you visualise how Bzip2, gzip, and xz are compressing different kinds of file types')
-    st.markdown('Here you can also see how the compression script operates:')
-with mid:
-    st.subheader('and',anchor=False)
-
-with right:
-    st.subheader(':orange[Information about Algorithms]',anchor=False)
-    st.markdown('Used in all the three compression programs.')
 
 
-
-
-st.image('images/Script.png')
-    
+user_session = _get_session()
+st.markdown(user_session)
 
 
