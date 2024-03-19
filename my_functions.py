0437c8dc1ef949815fb72cd511888d22b2ed2070
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-03-19 20:43:30 +0100

    Add files via upload

--- my_functions.py
+++ my_functions.py
@@ -216,3 +216,14 @@ def compression(methods, decomp_methods):
     my_bar.empty()
     st.success('results are done!', icon = '🥧')
     return 
+    
+    
+def _get_session():
+    from streamlit.runtime import get_instance
+    from streamlit.runtime.scriptrunner import get_script_run_ctx
+    runtime = get_instance()
+    session_id = get_script_run_ctx().session_id
+    session_info = runtime._session_mgr.get_session_info(session_id)
+    if session_info is None:
+        raise RuntimeError("Couldn't get your Streamlit Session object.")
+    return session_info.session
