2e1748a5b6b2a1af8964fe37c0b86d793895130d
KacperRuszczyk <66647202+KacperRuszczyk@users.noreply.github.com>
2024-05-24 16:24:24 +0200

    Add files via upload

--- ToClass/first take.py
+++ ToClass/first take.py
@@ -3,85 +3,14 @@
 funkcje snake
 constanty pathy i takie tam Takie_Tam
 '''
-class gzip:
-    def __init__(self,file_path):
-        self.file_path = file_path
-        
-    def compress(self,modifier=None):
-        start_time = time.time()
-        if self.modifier == None:
-            os.system(f'gzip {self.file_path}')
-        else:
-            os.system(f'gzip {self.modifier} {self.file_path}')
-        end_time = time.time()
-        resoult_time = end_time - start_time        
-        return resoult_time
-        
-    def decompress(self):
-        start_time = time.time()
-        os.system(f'gunzip {self.file_path}')
-        end_time = time.time()
-        resoult_time = end_time - start_time       
-        return resoult_time
-        
-class bzip2:
-    def __init__(self,file_path):
-        self.file_path = file_path
-        
-    def compress(self,modifier=None):
-        start_time = time.time()
-        if self.modifier == None:
-            os.system(f'bzip2 {self.file_path}')
-        else:
-            os.system(f'bzip2 {self.modifier} {self.file_path}')
-        end_time = time.time()
-        resoult_time = end_time - start_time        
-        return resoult_time
-        
-    def decompress(self):
-        start_time = time.time()
-        os.system(f'bzip2 -d {self.file_path}')
-        end_time = time.time()
-        resoult_time = end_time - start_time       
-        return resoult_time
-      
 
-class xz:
-    def __init__(self,file_path):
-        self.file_path = file_path
-        
-    def compress(self,modifier=None):
-        start_time = time.time()
-        if self.modifier == None:
-            os.system(f'xz {self.file_path}')
-        else:
-            os.system(f'xz {self.modifier} {self.file_path}')
-        end_time = time.time()
-        resoult_time = end_time - start_time        
-        return resoult_time
-        
-    def decompress(self):
-        start_time = time.time()
-        os.system(f'unxz {self.file_path}')
-        end_time = time.time()
-        resoult_time = end_time - start_time       
-        return resoult_time
-            
-        
         
         
 xz = 'unxz' , 'xz'       
 bzip2 = 'bzip2 -d' , 'bzip2'
 gzip = 'gunzip' , 'gzip' 
 
-files_list = [] #1
-comp_metode = [] #2
-file_size = [] #3
-comp_time = [] #4
-file_size_after_comp = [] #5
-decomp_time = [] #6
-file_size_after_decomp = [] #7
-check_if_diff = [] #8
+
 
 # Paths
 data_dir = '/mount/src/compressionsequel/work_space/data_dir'
@@ -91,57 +20,53 @@ results_dir = '/mount/src/compressionsequel/work_space/results_dir'
 uploaded_dir = '/mount/src/compressionsequel/work_space/uploaded_dir'        
         
         
-class Compression:
-    def __init__(self,File_Path):
-        self.File_Path = File_Path
-        
-    def compress_decompress(self,method):
-        start_time = time.time()
-        os.system(f'{self.method} {self.File_Path}')
-        end_time = time.time()
-        resoult_time = end_time - start_time        
-        return resoult_time
-        
-    
-    
+
 class FileManager:
     def __init__(slef,File_Path):
         self.File_Path = File_Path
+        self.files_list = [] #1
+        self.comp_metode = [] #2
+        self.file_size = [] #3
+        self.comp_time = [] #4
+        self.file_size_after_comp = [] #5
+        self.decomp_time = [] #6
+        self.file_size_after_decomp = [] #7
+        self.check_if_diff = [] #8
             
     def copy_files_from_to(self,files_from,files_to):
-        files_to_copy = os.listdir(self.files_from)
+        files_to_copy = os.listdir(files_from)
         for file_name in files_to_copy:
-            source_path = os.path.join(self.files_from, file_name)
-            destination_path = os.path.join(self.files_to, file_name)
+            source_path = os.path.join(files_from, file_name)
+            destination_path = os.path.join(files_to, file_name)
             shutil.copy(source_path, destination_path)
         return
         
     def move_one_file_from_to(self,file_from,file_to):
-            source_path = os.path.join(self.file_from, file_name)
-            shutil.move(source_path, self.file_to)
+            source_path = os.path.join(file_from, file_name)
+            shutil.move(source_path, file_to)
         return
     
     def move_all_files_from_to(self,files_from,files_to):
-        files_to_move = os.listdir(self.files_from)
+        files_to_move = os.listdir(files_from)
         for file_name in files_to_move:
-            source_path = os.path.join(self.files_from, file_name)
-            shutil.move(source_path, self.files_to)
+            source_path = os.path.join(files_from, file_name)
+            shutil.move(source_path, files_to)
         return
         
     def remove_file(self,path_with_file_name):
-        if os.path.isfile(self.path_with_file_name):
-            os.remove(self.path_with_file_name)
+        if os.path.isfile(path_with_file_name):
+            os.remove(path_with_file_name)
         return   
         
     def remove_all_files(self,Dir_Path)
-        for file_name in os.listdir(self.Dir_Path):
-            path_with_file_name = os.path.join(self.Dir_Path, file_name)
+        for file_name in os.listdir(Dir_Path):
+            path_with_file_name = os.path.join(Dir_Path, file_name)
             os.remove(path_with_file_name)
         return
         
-    def compress_decompress(self,method):
+    def compress_decompress(self,method,File_Path):
         start_time = time.time()
-        os.system(f'{self.method} {self.File_Path}')
+        os.system(f'{method} {File_Path}')
         end_time = time.time()
         resoult_time = end_time - start_time        
         return resoult_time
@@ -151,5 +76,13 @@ class FileManager:
         return File_Size
         
     def get_list_files_in_dir(self,Dir_Path):
-        File_List = os.listdir(self.Dir_Path)
+        File_List = os.listdir(Dir_Path)
         return File_List
+        
+        
+        
+        
+for File_Name in os.listdir(Dir_Path):
+    
+    for method in methods:
+        
